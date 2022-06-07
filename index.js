7ca8751253a807d229654c4e7b3b5c9106637b9d
Kipleting <kipletingedwin4@gmail.com>
2022-06-07 22:55:51 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,7 @@
+// Write your code here
+
+const divide=function(){ 
+    return 2000/100;
+}
+const square =(parameter1) => parameter1 * parameter1 ;
+const add=(parameter1,parameter2) =>parameter1 + parameter2;
